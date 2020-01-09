7f566ab01af3e7d8fdb7cf560ca8c0e21f27eedf
ankit039 <ryder.raj.ankit@gmail.com>
2020-01-09 22:54:49 +0530

    title updated

--- home.php
+++ home.php
@@ -5,7 +5,7 @@ if(isset($_SESSION["hashusername"])!= "")
 <html>
 <head>
   <meta charset="UTF-8">
-	<title>Quiz</title>
+	<title>XYZ</title>
   <link rel="icon" href="https://i.imgur.com/hmaZoKf.gif" type="image/gif">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="asset/css/style.css">
