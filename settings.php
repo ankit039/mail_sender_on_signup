5680693dee29580c8234a5f3675656edfb013c4c
ankit039 <ryder.raj.ankit@gmail.com>
2020-01-09 22:40:01 +0530

    guide added

--- settings.php
+++ settings.php
@@ -7,11 +7,12 @@ $useremail = $_SESSION["email"];
 $username_mail = "xyz@gmail.com";
 $username_pwd = "123";
 
+//to generate otp
 $otp = rand(100000,999999);
 $strotp = "$otp";
 
-$msg = "<h2>You are on your way! Lets confirm your E-Mail address!</h2><h3>As you can verify your account by clicking on here <a href='localhost/mailsenderverify.php?otp=$strotp&mail=$useremail'> click here</a></h3><h4>Thanks for making a account on NSS!<br>Regards Team NSS.</h4>";
-//<b><font color='red'>$strotp</font></b>
+//message which need to be send in email
+$msg = "<h2>You are on your way! Lets confirm your E-Mail address!</h2><h3>As you can verify your account by clicking on here <a href='localhost/mailsenderverify.php?otp=$strotp&mail=$useremail'> click here</a></h3><h4>Thanks for making a account on XYZ!<br>Regards Team XYZ.</h4>";
 
 $dbservername = "localhost";
 $dbusername = "root";
@@ -23,6 +24,4 @@ $conn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname);
 	$sql= "UPDATE logindetail SET otp='$strotp' WHERE email='$useremail'";
 	mysqli_query($conn,$sql);
 					
-
-
 ?>
